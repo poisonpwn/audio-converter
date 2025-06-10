21a09779cede689b5381fc21d4d898a61f048e7e
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2025-06-10 12:37:35 +0530

    feat: add trimming, combine both enpoints into a /process endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import BackgroundTasks, FastAPI, Form, File, HTTPException, UploadFile
+from datetime import time
+from fastapi import FastAPI, Form, File, HTTPException, UploadFile
 from typing import Annotated
 from pathlib import Path
 from tempfile import NamedTemporaryFile
@@ -17,46 +18,52 @@ CODEC_MAP = {
 }
 
 
-@app.post("/convert/")
-async def convert(
-    output_format: Annotated[str, Form()], file: Annotated[UploadFile, File()]
+@app.post("/process/")
+async def process(
+    input_file: Annotated[UploadFile, File()],
+    output_fmt: Annotated[str, Form()],
+    start: Annotated[time | None, Form()] = None,
+    end: Annotated[time | None, Form()] = None,
 ):
-    input_path = Path(file.filename) if file.filename is not None else Path()
-    if input_path.suffix is None:
-        raise HTTPException(status_code=422, detail=f"Unknown File Format")
+    trim_options = {}
+    if start is not None and end is not None:
+        if end <= start:
+            raise HTTPException(status_code=422, detail="end specified before start")
+        trim_options = {"ss": start.isoformat()[:8], "to": end.isoformat()[:8]}
 
-    if output_format not in CODEC_MAP or input_path.suffix[1:] not in CODEC_MAP:
+    input_path = (
+        Path(input_file.filename) if input_file.filename is not None else Path()
+    )
+    if input_path.suffix is None:
+        raise HTTPException(status_code=422, detail="Unknown File Format")
+
+    if (invalid_fmt := input_path.suffix[1:]) not in CODEC_MAP or (
+        invalid_fmt := output_fmt
+    ) not in CODEC_MAP:
         raise HTTPException(
-            status_code=422, detail=f"Unsupported File Format: {input_path.suffix}"
+            status_code=422, detail=f"Unsupported File Format: {invalid_fmt}"
         )
 
-    # the output file needs to be deleted as a background task, otherwise the file streaming cuts off.
     with (
-        NamedTemporaryFile() as input_file,
-        NamedTemporaryFile(delete=False, suffix=f".{output_format}") as output_file,
+        NamedTemporaryFile() as input_tmpfile,
+        NamedTemporaryFile(suffix=f".{output_fmt}", delete=False) as output_file,
     ):
         try:
-            input_file.write(await file.read())
-
+            input_tmpfile.write(await input_file.read())
             proc = (
                 FFmpeg()
                 .option("y")
-                .input(input_file.name)
-                .output(
-                    output_file.name,
-                    format=output_format,
-                    acodec=CODEC_MAP[output_format],
-                )
+                .input(input_tmpfile.name)
+                .output(output_file.name, acodec=CODEC_MAP[output_fmt], **trim_options)
             )
 
             await proc.execute()
+            return FileResponse(
+                output_file.name,
+                headers={
+                    "Content-Disposition": f"attachment; filename=converted.{output_fmt}"
+                },
+                background=BackgroundTask(output_file.close),
+            )
         except Exception:
-            return HTTPException(status_code=422, detail="Conversion Error")
-
-        return FileResponse(
-            output_file.name,
-            headers={
-                "Content-Disposition": f"attachment; filename=converted.{output_format}"
-            },
-            background=BackgroundTask(output_file.close),
-        )
+            raise HTTPException(status_code=412, detail="Conversion Error")
